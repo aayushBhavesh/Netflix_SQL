b7368808ca14f28d57f72a6aaedefddb5f05c0cf
AAYUSH SHAH <155434749+aayushBhavesh@users.noreply.github.com>
2025-12-28 12:28:18 +0530

    Update Netflix business problems list with new queries

--- Business Problems Netflix.sql
+++ Business Problems Netflix.sql
@@ -1,4 +1,4 @@
--- 15 Business Problems & Solutions
+--  Business Problems & Solutions
 
 1. Count the number of Movies vs TV Shows
 2. Find the most common rating for movies and TV shows
@@ -15,10 +15,34 @@ return top 5 year with highest avg content release!
 12. Find all content without a director
 13. Find how many movies actor 'Salman Khan' appeared in last 10 years!
 14. Find the top 10 actors who have appeared in the highest number of movies produced in India.
-15.
-Categorize the content based on the presence of the keywords 'kill' and 'violence' in 
+15. Categorize the content based on the presence of the keywords 'kill' and 'violence' in 
 the description field. Label content containing these keywords as 'Bad' and all other 
 content as 'Good'. Count how many items fall into each category.
+16. Find the year-over-year growth rate of Netflix content additions.
+17. Identify which month has the highest number of content additions historically.
+18. Determine whether Netflix is shifting more towards TV Shows or Movies over time.
+19. Find the average gap (in days) between release year and date added for content.
+20. Identify years where TV Shows surpassed Movies in total additions.
+21. Find the top 3 content-producing countries per year.
+22. Calculate the percentage contribution of India’s content to total Netflix content each year.
+23. Identify countries whose content presence declined year-over-year.
+24. Find countries that only produce Movies but no TV Shows.
+25. Rank countries by average movie duration.
+26.Find the median movie duration instead of average.
+27. Identify TV shows with irregular season gaps (non-continuous season counts).
+28. Find movies that are outliers in duration (too long or too short using percentile logic).
+29. Compare average duration of Indian movies vs Non-Indian movies.
+30. Find directors whose movies consistently exceed average duration.
+31. dentify directors who worked across multiple countries.
+32. Find actors who transitioned from movies to TV shows over time.
+33. Rank actors by career longevity on Netflix (first appearance to latest).
+34. Find directors who frequently collaborate with the same actors.
+35. Identify actors whose content is evenly split between movies and TV shows.
+36.Find genres that are growing fastest in the last 5 years.
+37. Identify genres that are declining or stagnant.
+38. Find content descriptions that contain more than 3 genre-related keywords.
+39. Compare average duration per genre.
+40. Identify genres that are country-specific (exist in one country only).
 
 
 
